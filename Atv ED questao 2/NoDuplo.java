c8f4ba9ca41e4dbacd89524bac63c7f6afb742a9
Kazunorl27 <nakamotokazu208@gmail.com>
2025-10-03 14:03:15 -0300

    Create NoDuplo.java

---
+++ Atv ED questao 2/NoDuplo.java
@@ -0,0 +1,11 @@
+class NoDuplo {
+    int valor;
+    NoDuplo anterior;
+    NoDuplo proximo;
+
+    public NoDuplo(int valor) {
+        this.valor = valor;
+        this.anterior = null; 
+        this.proximo = null;
+    }
+}
