9442fd3f150795bf8982a30eeeef6b78e64a0f55
Kazunorl27 <nakamotokazu208@gmail.com>
2025-10-19 16:41:02 -0300

    Create ClassNo.java

---
+++ ArvoreATV3/ClassNo.java
@@ -0,0 +1,12 @@
+class No {
+    int valor;
+    int altura; 
+    No esquerda, direita;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.altura = 1; 
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
