32190009cb393f0091ea827d6e01ccbaf8a0cc2a
Kazunorl27 <nakamotokazu208@gmail.com>
2025-10-19 16:08:29 -0300

    Fix indentation in rotacaoDireita method

---
+++ ArvoresATV2/NoAVLRotacaoDireita.java
@@ -0,0 +1,12 @@
+        private NoAVL rotacaoDireita(NoAVL y) {
+        NoAVL x = y.esquerda;
+        NoAVL T2 = x.direita;
+
+        x.direita = y;
+        y.esquerda = T2;
+
+        atualizarAltura(y);
+        atualizarAltura(x);
+
+        return x; 
+    }
